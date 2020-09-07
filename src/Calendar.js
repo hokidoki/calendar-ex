f3e473ca5876e27bab14dea3d8629c664c969cad
hokey_imac <substantiation@naver.com>
2020-09-08 00:08:07 +0900

    add try/catch in DateHeader

--- src/Calendar.js
+++ src/Calendar.js
@@ -13,28 +13,32 @@ class DateHeader extends Component {
   }
 
   mapArrayToDate = (dateArray) => {
-    if(dateArray.length !== 7){
-      console.log(new Error("dates props must be had 7 date"))
-      dateArray = ["일", "월", "화", "수", "목", "금", "토"]
-    }
-    
-    return dateArray.map((date, index) => {
-      const className = ()=>{
-        let className = "RCA-calendar-date-component";
-        if(index === 0){
-          return className + " date-sun"
-        }else if(index === 6){
-          return className + " date-sat"
-        }else{
-          return className + " date-weekday"
-        }
+    try{
+      if(dateArray.length !== 7){
+        console.log(new Error("dates props must be had 7 date"))
+        dateArray = ["일", "월", "화", "수", "목", "금", "토"]
       }
-      return (
-        <div className={className()} key={"RCA-header-"+date}>
-          {date}
-        </div>
-      )
-    })
+      
+      return dateArray.map((date, index) => {
+        const className = ()=>{
+          let className = "RCA-calendar-date-component";
+          if(index === 0){
+            return className + " date-sun"
+          }else if(index === 6){
+            return className + " date-sat"
+          }else{
+            return className + " date-weekday"
+          }
+        }
+        return (
+          <div className={className()} key={"RCA-header-"+date}>
+            {date}
+          </div>
+        )
+      })
+    }catch{
+      throw new Error("date must be string ")
+    }
   }
 
   render() {
